7003ee25b5a9c607cfcbd1d19260b94cf5dc53f0
Admin <admin@Admins-MacBook-Air.local>
2023-10-02 15:30:15 +0100

    completed challenge 4) /api/articles/id

---
+++ models/articles.models.js
@@ -0,0 +1,20 @@
+const db = require('../db/connection.js')
+const format = require('pg-format');
+
+
+function fetchArticleByID(article_id) {
+    console.log('you are in models')
+    return db
+    .query('SELECT * FROM articles WHERE article_id = $1',[article_id])
+    .then((result) => {
+        console.log(result)
+        if(result.rows.length ===0) {
+            return Promise.reject({status:404, msg: "article does not exist"})
+        }
+        else {
+            return result.rows[0];
+        } 
+    })
+}
+
+module.exports = fetchArticleByID;
