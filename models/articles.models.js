af967ed086893edee916d9e3f19106da45127dbf
Admin <admin@Admins-MacBook-Air.local>
2023-10-02 15:32:12 +0100

    removed console logs for challenge 4

--- models/articles.models.js
+++ models/articles.models.js
@@ -3,7 +3,6 @@ const format = require('pg-format');
 
 
 function fetchArticleByID(article_id) {
-    console.log('you are in models')
     return db
     .query('SELECT * FROM articles WHERE article_id = $1',[article_id])
     .then((result) => {
